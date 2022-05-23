2aadd2dfcbf235cdb571ddef33e66c853a58e3b6
sewakgautam <sewak.gautam58@gmail.com>
2022-05-23 08:14:22 +0545

    basic js

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-setInterval(function () {
-  let s = document.getElementById("dot").getAttribute("style");
-  if (s == "font-family: sans-serif;") {
-    document.getElementById("dot").style.fontFamily = "Trebuchet";
-  } else {
-    document.getElementById("dot").style.fontFamily = "sans-serif";
-  }
-}, 1000);
